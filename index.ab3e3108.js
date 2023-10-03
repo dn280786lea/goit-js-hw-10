7a7609eae54379157f4bdaed6ae96702d4d872f8
dn280786lea <132853931+dn280786lea@users.noreply.github.com>
2023-10-03 08:27:57 +0000

    Deploying to gh-pages from @ dn280786lea/goit-js-hw-10@bf234b9ef7e1207f23491e32fe041521963bbbed 🚀

--- index.ab3e3108.js
+++
@@ -1,2 +0,0 @@
-!function(){var e="live_hsz8LgxGKg2EdraBaRAoqT1NfEDzIXDGDgW167CQBZhFlPZGlomO9Uew9cUHBXoV",t="https://api.thecatapi.com/v1";var n=document.querySelector(".breed-select");fetch("".concat(t,"/").concat("breeds","?api_key=").concat(e)).then((function(e){if(!e.ok)throw new Error(e.status);return e.json()})).then((function(e){!function(e){e.forEach((function(e){var t=document.createElement("option");t.value=e.id,t.textContent=e.name,n.appendChild(t)}))}(e)})).catch((function(e){console.error("Error fetching breeds:",e)}))}();
-//# sourceMappingURL=index.ab3e3108.js.map
