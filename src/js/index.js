e134f82e6872835667bf42ad7df78561bcd1af78
dn280786lea <ekaterina.lubenets@gmail.com>
2023-10-04 08:59:34 +0200

    1
    1

--- src/js/index.js
+++ src/js/index.js
@@ -7,7 +7,8 @@ breedSelect: document.querySelector('.breed-select'),
 catImage: document.querySelector('.cat-inform'),
 loader: document.querySelector('.loader'),
 error: document.querySelector('.error'),
-  }
+}
+  
 const { breedSelect, catImage, loader, error } = ref;
 
 let arrBreedsId = [];
@@ -17,13 +18,12 @@ fetchBreeds()
         arrBreedsId.push({text: element.name, value: element.id});
     });
     new SlimSelect({
-        select: selector,
+        select: breedSelect,
         data: arrBreedsId
     });
-    })
-catch(onFetchError);
-
-selector.addEventListener('change', onSelectBreed);
+})
+ 
+breedSelect.addEventListener('change', onSelectBreed);
 
 function onSelectBreed(event) {
     loader.classList.replace('is-hidden', 'loader');
@@ -34,24 +34,20 @@ function onSelectBreed(event) {
     fetchCatByBreed(selectedBreedId)
     .then(data => {
         breedSelect.classList.replace('loader', 'is-hidden');
-        selector.classList.remove('is-hidden');
+        breedSelect.classList.remove('is-hidden'); // Замість selector використовуйте breedSelect
         const { url, breeds } = data[0];
         
         catImage.innerHTML = `<div class="box-img"><img src="${url}" alt="${breeds[0].name}" width="400"/></div><div class="box"><h1>${breeds[0].name}</h1><p>${breeds[0].description}</p><p><b>Temperament:</b> ${breeds[0].temperament}</p></div>`
         catImage.classList.remove('is-hidden');
     })
     .catch(onFetchError);
-};
+}
 
 function onFetchError(error) {
     breedSelect.classList.remove('is-hidden');
     loader.classList.replace('loader', 'is-hidden');
 
-    Notify.failure('Oops! Something went wrong! Try reloading the page or select another cat breed!', {
-        position: 'center-center',
-        timeout: 5000,
-        width: '400px',
-        fontSize: '24px'
+    Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page or select another cat breed!', {
     });
 };
    
